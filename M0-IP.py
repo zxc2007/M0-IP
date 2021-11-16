9051c6c44af4c1affff17872e9ae3a208d512400
Mo Code <92459793+mocode-it@users.noreply.github.com>
2021-11-16 08:04:15 +0200

    Update and rename M0-IP.pyc to M0-IP.py

---
+++ M0-IP.py
@@ -0,0 +1,46 @@
+#--------------------------->
+#Coded By Mokhtar Abdelkreem
+#-------------------------->
+#Librarys
+import requests
+import os
+import time
+import webbrowser
+#--------------
+
+print('\033[1;31m Wait Please ...')
+time.sleep(3)
+os.system('clear')
+os.system('xdg-open https://cutt.us/mocode')
+webbrowser.open('https://cutt.us/mocode')
+#Banner
+print('''
+\033[1;35m
+
+_______________________________
+|  |_|  |       |  |   |       |
+|       |  ___  |  |   |    _
+|       | |▀-▀| |  |   |   |_| |
+|       | |___| |  |   |    ___|
+| ||_|| |       |  |   |   |
+|_|   |_|_______|  |___|____
+
+\033[1;36m
+★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+
+ Devolper : Mokhtar Abdelkreem
+
+ Inf Dev  : https://dev.page/mokhtar
+
+ Channel  : https://cutt.us/mocode
+
+★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+''')
+
+api ="http://ip-api.com/"
+mocode = input("\033[1;34mEnter IP Victim :")
+result= requests.get(api+mocode)
+print(result.text)
+
+
+
